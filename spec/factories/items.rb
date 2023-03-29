5b0fc6868c784332d705faa4eef061f4dbb52b19
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-28 19:57:22 -0600

    Feat: add functionlaity to return all of a merchants items through an API call

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Appliance.equipment}
+    description { Faker::Appliance.brand }
+    unit_price { Faker::Number.decimal(l_digits: 2) }
+    merchant_id { 1 }
   end
 end
