0787f1bd7ec373fc36acb4ac871c174957dd2dc7
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-28 19:03:09 -0600

    Feat: add functionality and tests for returning a specific merchant from an api call

--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -1,19 +1,34 @@
 require "rails_helper"
 
 describe "Merchants API" do 
+  before :each do 
+    @list = create_list(:merchant, 3)
+  end 
   it "Sends a list of merchants" do 
-    create_list(:merchant, 3)
 
     get "/api/v1/merchants"
 
     expect(response.status).to eq(200)
 
     merchants = JSON.parse(response.body, symbolize_names: true)
- 
+
     expect(merchants[:data].count).to eq(3)
 
     merchants[:data].each do |merchant|
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it "finds a specific merchant" do 
+
+    get "/api/v1/merchants/#{@list.first.id}"
+
+    expect(response.status).to eq(200)
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant.count).to eq(1)
+
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
