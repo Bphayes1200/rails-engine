f852ab8dcc596fd8b48e1ddd3a0e6b8e4bca3736
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-30 16:04:40 -0600

    Feat: create find item by name method for class item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   belongs_to :merchant
+
+  def self.find_item_by_name(name)
+
+    self.where(name: name).order(:name)
+  end
 end 
