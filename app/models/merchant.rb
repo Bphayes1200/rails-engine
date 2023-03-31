a14569e41fc3705a480650308708cc16c7f9f60f
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-30 23:25:21 -0600

    Feat: add find all merchants by name method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,9 @@ class Merchant < ApplicationRecord
 	has_many :invoice_items, through: :items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+  
+  def self.find_all_merchants_by_name(name)
+    self.where(name: name).order(:name)
+  end 
 end
+
